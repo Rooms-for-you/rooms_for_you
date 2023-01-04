7efc8fb213a88a82b9114189cc96547ce6b84045
thiagonazarko <thnzk1@gmail.com>
2023-01-04 10:22:57 -0300

    fix-models-serializers-names

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
-from addresses.models import Addresses
 
-class AddressesSerializer(serializers.Serializer):
+class AddressSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     street = serializers.CharField(max_length=255)
     number = serializers.IntegerField(max_length=10)
