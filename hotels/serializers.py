8570f7421a9a3e9c6c669e30cd09d44ba42c1a97
Victor Gois <victoraugusto340@gmail.com>
2023-01-05 16:39:35 -0300

    feedbacks funcionando

--- hotels/serializers.py
+++ hotels/serializers.py
@@ -2,20 +2,21 @@ from rest_framework import serializers
 from .models import Hotel
 from addresses import seralizers
 from addresses.models import Address
+from feedbacks.serializers import FeedbackSerializer
 import ipdb
+
 
 class HotelSerializer(serializers.ModelSerializer):
     address = seralizers.AddressSerializer()
+    feedbacks = FeedbackSerializer(many=True)
 
     class Meta:
         model = Hotel
-        fields = ["id", 'owner', 'address']
-        read_only_fields = ['owner']
+        fields = ["id", "owner", "address", "feedbacks"]
+        read_only_fields = ["owner", "feedbacks"]
 
     def create(self, validated_data):
-     address_dict =  validated_data.pop("address")
-     address_obj = Address.objects.create(**address_dict)
-     hotel_obj = Hotel.objects.create(**validated_data, address=address_obj)
-     return hotel_obj
-     
-
+        address_dict = validated_data.pop("address")
+        address_obj = Address.objects.create(**address_dict)
+        hotel_obj = Hotel.objects.create(**validated_data, address=address_obj)
+        return hotel_obj
--- hotels/urls.py
+++ hotels/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from feedbacks.views import ListCreateFeedback
 
 urlpatterns = [
     path("hotels/", views.ListCreateHotelView.as_view()),
     path("hotels/<int:pk>/", views.RetrieveHotelView.as_view()),
+    path("hotels/<int:pk>/feedbacks/", ListCreateFeedback.as_view()),
 ]
