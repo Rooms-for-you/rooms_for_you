d3dfb949ac96e97eab91f7fee800dcb9517b927b
kalebesampaio <kalebesmp2002@gmail.com>
2023-01-10 14:24:34 -0300

    fix: add hotel and room update destroy

--- rooms/views.py
+++ rooms/views.py
@@ -21,10 +21,19 @@ class RoomView(generics.ListCreateAPIView, PageNumberPagination):
         hotel = get_object_or_404(Hotel, pk=hotel_id)
         serializer.save(hotel=hotel)
 
+    def get_queryset(self):
+        hotel_id = self.kwargs["pk"]
+        hotel = get_object_or_404(Hotel, pk=hotel_id)
+
+        queryset = Room.objects.filter(hotel=hotel)
+
+        return queryset    
+
 class RoomDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
+
     
